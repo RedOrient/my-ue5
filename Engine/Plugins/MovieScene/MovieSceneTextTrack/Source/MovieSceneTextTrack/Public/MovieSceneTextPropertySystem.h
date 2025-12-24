9525afa5bcc1e562136aa4008e75bd552ea5c083
unknown <zhudongfang@slhd.com>
2025-12-24 19:53:43 +0800

    add file

--- Engine/Plugins/MovieScene/MovieSceneTextTrack/Source/MovieSceneTextTrack/Public/MovieSceneTextPropertySystem.h
+++
@@ -1,9 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#pragma once
-
-#include "Misc/CoreMiscDefines.h"
-
-/** Moved to MovieSceneTracks module */
-UE_DEPRECATED_HEADER(5.7, "Use Systems/MovieSceneTextPropertySystem.h (MovieSceneTracks module) instead")
-#include "Systems/MovieSceneTextPropertySystem.h"
